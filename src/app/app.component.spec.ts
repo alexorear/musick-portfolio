0219c45ef99aeb32ad1611a3a4021827f2dc181d
alexorear <alex.orear@gmail.com>
2018-05-14 20:40:58 -0500

    Add app component test imports. Add test for nav links.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
+import { NavigationComponent } from './core/header/navigation/navigation.component';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [
 				AppComponent,
-				HeaderComponent
+				HeaderComponent,
+				NavigationComponent
 			],
 		}).compileComponents();
 	}));
