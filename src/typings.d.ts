be4ba9b2355840b784b1e8d665431ced5a7ed36e
alexorear <alex.orear@gmail.com>
2018-05-22 21:35:18 -0500

    Create PortfolioExample type.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,14 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
 interface NodeModule {
-  id: string;
+	id: string;
+}
+
+interface PortfolioExample {
+	id: number;
+	category: string;
+	title: string;
+	description: string;
+	primaryUrl: string;
+	primaryThumbUrl: string;
 }
