faf39c3e90f45e0405862b4764ddd6d6c081e152
alexorear <alex.orear@gmail.com>
2018-05-22 21:36:30 -0500

    Create PortfioExamples json data.

---
+++ src/app/data/porfolio-examples.ts
@@ -0,0 +1,75 @@
+export const portfolioExamples: PortfolioExample[] = [
+	{
+		id: 1,
+		category: 'print',
+		title: 'Jen’s Resale Boutique',
+		description: 'A fun, upbeat consignment shop catered to everyone ages 13 to 100!',
+		primaryUrl: 'assets/images/portfolio-examples/logos/JenBCard_Full.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/logos/thumbnails/JensResale.jpb'
+	},
+	{
+		id: 2,
+		category: 'print',
+		title: 'Creature Curios',
+		description: 'These odd, yet adorable one-eyed stuffed animals are hand stitched with love by a local St. Louis artist.',
+		primaryUrl: 'assets/images/portfolio-examples/logos/CeatureCuriosLabel.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/logos/thumbnails/CreatureCurios.jpg'
+	},
+	{
+		id: 3,
+		category: 'print',
+		title: 'Podbelsek Family Creations',
+		description: 'A family of antique furniture flippers who love to shabby chick and bring new life to their labors of love.',
+		primaryUrl: 'assets/images/portfolio-examples/logos/PodFamBCard_Full2.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/logos/thumbnails/Podbelsek.jpg'
+	},
+	{
+		id: 4,
+		category: 'print',
+		title: 'AAA Alterations &amp; Tailoring',
+		description: 'This alteration shop revamped the vintage sign in front of their location and I used it ' +
+		'as the inspiration for their whole company’s branding.',
+		primaryUrl: 'assets/images/portfolio-examples/logos/AAABCard_Full.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/logos/thumbnails/AAA.jpg'
+	},
+	{
+		id: 5,
+		category: 'print',
+		title: 'Optimus Talent Partners',
+		description: 'A talent management company helping your current and future team soar to new heights.',
+		primaryUrl: 'assets/images/portfolio-examples/logos/Optimus_Full.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/logos/thumbnails/Optimus.jpg'
+	},
+	{
+		id: 6,
+		category: 'print',
+		title: 'Jen’s Resale Boutique',
+		description: 'A fun, upbeat consignment shop catered to everyone ages 13 to 100!',
+		primaryUrl: 'assets/images/portfolio-examples/print/',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/'
+	},
+	{
+		id: 7,
+		category: 'misc',
+		title: '',
+		description: '',
+		primaryUrl: 'assets/images/portfolio-examples/misc/Bug_Full-01.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/misc/thumbnails/Bugs.jpg'
+	},
+	{
+		id: 8,
+		category: 'misc',
+		title: '',
+		description: '',
+		primaryUrl: 'assets/images/portfolio-examples/misc/bugs.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/misc/thumbnails/PaperBugs.jpg'
+	},
+	{
+		id: 9,
+		category: 'misc',
+		title: '',
+		description: '',
+		primaryUrl: 'assets/images/portfolio-examples/misc/organs_copy.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/misc/thumbnails/Organs.jpg'
+	}
+];
