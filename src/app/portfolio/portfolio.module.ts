cdd37dee6ea282fcacd68af05080ba31dc23de5d
alexorear <alex.orear@gmail.com>
2018-05-22 22:34:45 -0500

    Provide portfolio example service in module.

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { PortfolioExamplesService } from '../services/portfolio-examples.service';
 import { LogoDesignComponent } from './logo-design/logo-design.component';
 import { MiscWorkComponent } from './misc-work/misc-work.component';
 import { OverviewComponent } from './overview/overview.component';
@@ -16,6 +17,7 @@ import { PrintDesignComponent } from './print-design/print-design.component';
 		PrintDesignComponent,
 		OverviewComponent
 	],
+	providers: [PortfolioExamplesService],
 	exports: [
 		OverviewComponent
 	]
