814e7d9604fbb56897ec164e0c17e1f85a653488
alexorear <alex.orear@gmail.com>
2018-05-27 10:58:01 -0500

    Add test for service.

--- src/app/services/portfolio-examples.service.spec.ts
+++ src/app/services/portfolio-examples.service.spec.ts
@@ -1,15 +1,46 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { Observable } from 'rxjs/Observable';
+
+import { Portfolio } from '../data/porfolio-examples';
 import { PortfolioExamplesService } from './portfolio-examples.service';
 
 describe('PortfolioExamplesService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [PortfolioExamplesService]
-    });
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [PortfolioExamplesService]
+		});
+	});
 
-  it('should be created', inject([PortfolioExamplesService], (service: PortfolioExamplesService) => {
-    expect(service).toBeTruthy();
-  }));
+	it('should be created', inject([PortfolioExamplesService], (service: PortfolioExamplesService) => {
+		expect(service).toBeTruthy();
+	}));
+
+	describe('service methods', () => {
+		let service: PortfolioExamplesService;
+
+		beforeEach(() => {
+			service = new PortfolioExamplesService;
+		});
+
+		describe('get portfolioExamples', () => {
+			it('should return an observable of all portfolio examples', () => {
+				expect(service.portfolioExamples).toEqual(Observable.of(Portfolio));
+			});
+		});
+
+		describe('getPortfolioPiece', () => {
+			it('should return a single portfolio example', () => {
+				const expected = {
+					id: 1,
+					category: 'logo',
+					title: 'Jen’s Resale Boutique',
+					description: 'A fun, upbeat consignment shop catered to everyone ages 13 to 100!',
+					primaryUrl: 'assets/images/portfolio-examples/logos/JenBCard_Full.jpg',
+					primaryThumbUrl: 'assets/images/portfolio-examples/logos/thumbnails/JensResale.jpb'
+				};
+				expect(service.getPortfolioPiece(1)).toEqual(Observable.of(expected));
+			});
+		});
+	});
 });
