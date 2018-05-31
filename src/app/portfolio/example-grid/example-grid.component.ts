0c977d84bd3e197e8fc985787d4323b861d6aeb8
alexorear <alex.orear@gmail.com>
2018-05-30 21:05:03 -0500

    Create currentSelection input. logoExamples$ => portfolioExamples. OnChanges switch statement to pull correct portfolio examples.

--- src/app/portfolio/example-grid/example-grid.component.ts
+++ src/app/portfolio/example-grid/example-grid.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
+import { PortfolioSection } from '../types/portfolio-section';
 import { PortfolioExamplesService } from '../../services/portfolio-examples.service';
 
 @Component({
@@ -8,15 +9,26 @@ import { PortfolioExamplesService } from '../../services/portfolio-examples.serv
 	templateUrl: './example-grid.component.html',
 	styleUrls: ['./example-grid.component.scss']
 })
-export class ExampleGridComponent implements OnInit {
-	logoExamples$: Observable<PortfolioExample[]>;
+export class ExampleGridComponent implements OnChanges {
+	@Input() currentSelection: PortfolioSection;
+	portfolioExamples$: Observable<PortfolioExample[]>;
 
 	constructor(
 		private portfolioApi: PortfolioExamplesService
 	) { }
 
-	ngOnInit() {
-		this.logoExamples$ = this.portfolioApi.getLogoExamples();
+	ngOnChanges() {
+		switch (this.currentSelection) {
+			case 'logo':
+				this.portfolioExamples$ = this.portfolioApi.getLogoExamples();
+				break;
+			case 'print':
+				this.portfolioExamples$ = this.portfolioApi.getPrintExamples();
+				break;
+			case 'misc':
+			this.portfolioExamples$ = this.portfolioApi.getMiscExamples();
+				break;
+		}
 	}
 
 }
