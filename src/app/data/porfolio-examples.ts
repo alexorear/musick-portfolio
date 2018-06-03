e6ffd005ff52d1d41126ce8d75eb977014ef6f3e
alexorear <alex.orear@gmail.com>
2018-06-03 18:45:39 -0500

    Add print example objects.

--- src/app/data/porfolio-examples.ts
+++ src/app/data/porfolio-examples.ts
@@ -26,7 +26,7 @@ export const Portfolio: PortfolioExample[] = [
 	{
 		id: 4,
 		category: 'logo',
-		title: 'AAA Alterations &amp; Tailoring',
+		title: 'AAA Alterations & Tailoring',
 		description: 'This alteration shop revamped the vintage sign in front of their location and I used it ' +
 		'as the inspiration for their whole company’s branding.',
 		primaryUrl: 'assets/images/portfolio-examples/logos/AAABCard_Full.jpg',
@@ -63,5 +63,85 @@ export const Portfolio: PortfolioExample[] = [
 		description: '',
 		primaryUrl: 'assets/images/portfolio-examples/misc/organs_copy.jpg',
 		primaryThumbUrl: 'assets/images/portfolio-examples/misc/thumbnails/Organs.jpg'
+	},
+	{
+		id: 10,
+		category: 'print',
+		title: '2016 Belle Ringer Invitation',
+		description: 'The most prestigious guest golf event of the year calls for premier invitations.',
+		primaryUrl: 'assets/images/portfolio-examples/print/BelleRinger_Full-01.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/BelleRinger.jpg'
+	},
+	{
+		id: 11,
+		category: 'print',
+		title: 'Karner-Bessigano Wedding Invitation',
+		description: 'Guests of this Beauty and The Beast themed wedding received this book-styled invitation, complete with RSVP library card.',
+		primaryUrl: 'assets/images/portfolio-examples/print/KateWedding_Full-01.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/BessiganoWedding.jpg'
+	},
+	{
+		id: 12,
+		category: 'print',
+		title: '2014 Past Presidents Day Menu',
+		description: 'Each and every piece of these 40 dinner menus was constructed by hand including the torn edges, hand-made tassels, and the binding.',
+		primaryUrl: 'assets/images/portfolio-examples/print/PastPresidentsMenu.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/PastPresMenu.jpg'
+	},
+	{
+		id: 13,
+		category: 'print',
+		title: 'New Member Cocktail Party',
+		description: 'A first attempt at calligraphy and at creating Cricut cut files turned into the standard design for the quarterly new member cocktail party.',
+		primaryUrl: 'assets/images/portfolio-examples/print/NewMemberCocktail_Full-01.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/NewMember.jpg'
+	},
+	{
+		id: 14,
+		category: 'print',
+		title: '2015 New Year’s Eve Invitation',
+		description: 'These fun, hat shaped invites were cut by hand and sent to over 500 people encouraging them to ring in the new year at their club. This invitation more than doubled the event’s normal attendance, and because of this increase in attendees I was awarded a bonus.',
+		primaryUrl: 'assets/images/portfolio-examples/print/NewYearsHat_Full-01.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/NewYears2015.jpg'
+	},
+	{
+		id: 15,
+		category: 'print',
+		title: 'McLeod-Konrad Wedding Invitation',
+		description: 'This picturesque fall wedding invitation features two lovebirds and a photo of the groom’s childhood backyard where the wedding took place under the large tree on the right.',
+		primaryUrl: 'assets/images/portfolio-examples/print/AmandaWeddingInvite2.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/KonradWedding.jpg'
+	},
+	{
+		id: 16,
+		category: 'print',
+		title: '2016 New Year’s Eve Invitation',
+		description: 'While looking for a different way to send an event invitation, I created this folded postcard format, increasing overall attendance and saving money on postage.',
+		primaryUrl: 'assets/images/portfolio-examples/print/NewYearsPostcard_Full-01.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/NewYears2016.jpg'
+	},
+	{
+		id: 17,
+		category: 'print',
+		title: 'Everything Is Design',
+		description: 'This accordion fold booklet, which includes excerpts from Paul Rand’s Everything Is Design, was accepted into the 2010 SIUE Juried Undergraduate Show.',
+		primaryUrl: 'assets/images/portfolio-examples/print/2_Accord_Fold.png',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/Accordion.jpg'
+	},
+	{
+		id: 18,
+		category: 'print',
+		title: 'Board of Governor’s Annual Christmas Dinner',
+		description: 'This traditional invitation, with matching place cards, was designed to encapsulate the spirit of the season for our annual Board Christmas dinner.',
+		primaryUrl: 'assets/images/portfolio-examples/print/BoardXmasInvite.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/BoardXmas.jpg'
+	},
+	{
+		id: 19,
+		category: 'print',
+		title: 'Bridal Shower Elizabeth Oltman',
+		description: 'This Kate Spade inspired invitation set the mood for the bride-to-be’s special day with iconic stripes and fun florals.',
+		primaryUrl: 'assets/images/portfolio-examples/print/LiddyShowerInvite.jpg',
+		primaryThumbUrl: 'assets/images/portfolio-examples/print/thumbnails/OltmanShower.jpg'
 	}
 ];
