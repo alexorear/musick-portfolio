0e286f8137bd2ccf6ceafc73ce7cbe797ae2b310
alexorear <alex.orear@gmail.com>
2018-06-05 20:01:40 -0500

    Scroll window to top of navbar when navigation changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+
+import 'rxjs/add/operator/filter';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+	constructor(
+		private router: Router,
+		private route: ActivatedRoute
+	) { }
+
+	ngOnInit() {
+		this.router.events.filter((event) => event instanceof NavigationEnd).subscribe(() => {
+			// scroll to top of nav bar when navigating to new page
+			const content = document.getElementById('content');
+			window.scrollTo({
+				'behavior': 'smooth',
+				'left': 0,
+				'top': content.offsetTop - 70
+			});
+		});
+
+	}
 }
